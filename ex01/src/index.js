8d39b4d98d0b1d03d15ffc3cb8b557097285b8ae
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-11 07:35:13 +0100

    Added lunchjson

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,6 +1,17 @@
+mkdir ex01
+npm init - y // Initialize project in Day03
+console.log('Hello World!');
+code. // Open VSC in this folder
+
+
+cd ex01
+code ex01 // Opens VSC in ex01 folder
+
+
+
 function main() {
     // Only cchange code below this line 
-    console.log("Hello World!");
+    console.log('Hello World!');
     // Only change the code above this line
 }
 
