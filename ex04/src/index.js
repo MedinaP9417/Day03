b997bf48ade1aa3e086bb2fea18ba01c2e6d186c
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-11 08:37:53 +0100

    Assign x to y

---
+++ ex04/src/index.js
@@ -0,0 +1,22 @@
+drawCats = 5;
+
+myCat = 5;
+ourCats = myCat;
+
+
+function main() {
+    // Only cchange code below this line 
+
+    // Only change the code above this line
+}
+return [x, y];
+let x = 10;
+let y = 10;
+
+
+console.log(x = y); // x is 10
+
+
+
+
+module.exports = main;
