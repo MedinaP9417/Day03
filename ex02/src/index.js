3af372c1d5c9ef7459b0e49adc27971e89a2f02a
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-11 08:05:33 +0100

    Making Comments

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+//Making comments in index.js first
+
+
+function main() {
+    // Only cchange code below this line 
+    console.log('Hello World!');
+    // Only change the code above this line
+}
+
+/*main();*/
+
+module.exports = main;
