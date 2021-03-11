45b81c86344ea3e995267a65be57a1c2ce5c8b0f
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-11 08:09:37 +0100

    var fooBar

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+//Making comments in index.js first
+
+
+function main() {
+    // Only cchange code below this line 
+    console.log('Hello World!');
+    // Only change the code above this line
+}
+var fooCat;
+
+/*main();*/
+
+module.exports = main;
